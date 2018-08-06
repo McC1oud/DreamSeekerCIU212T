6a3d05476d3f700f77c04e4ae00dda0e1298dd78
McC1oud <neon__fox@hotmail.com>
2018-08-06 17:00:20 +1000

    CameraWork
    ChaseCam fixes, still continuing

--- DreamSeeker_Proto/Assets/CamerTrack.cs
+++ DreamSeeker_Proto/Assets/CamerTrack.cs
@@ -4,14 +4,46 @@ using UnityEngine;
 
 public class CamerTrack : MonoBehaviour {
 
+    public float cameraHeight = 4.5f;
+
+    public float cameraEaseDistance;
+
+
     public GameObject target;
-	// Use this for initialization
-	void Start () {
+
+    public float smoothTime;
+    private Vector3 velocity = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+
         transform.LookAt(target.transform);
+        Vector3 aV = transform.position;
+        Vector3 bV = target.transform.position * cameraEaseDistance;
+        //Vector3 newV = Vector3.
+
+        //Vector3 offSetVector = Vector3.Distance(aV,bV);
+
+        if(Vector3.Distance(transform.position, target.transform.position) > cameraEaseDistance)
+        {
+            print("movecameplz");
+            transform.position = Vector3.SmoothDamp(transform.position, target.transform.position, ref velocity, smoothTime);
+            Vector3 defHeight = transform.position;
+            defHeight.y = cameraHeight;
+            transform.position = defHeight;
+        }
+
+
+        if (Vector3.Distance(transform.position, target.transform.position) <= cameraEaseDistance)
+        {
+            print("stopcameplz");
+        }
+
+
     }
 }
